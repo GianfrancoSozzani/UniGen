83953b2294365ba0cda3708aa0e5cc75e6ca01b9
Daniele Pagano <danibari1@hotmail.com>
2025-04-23 17:58:11 +0200

    Aggiunte Classi: Prove, Test_DA, Test_DC.

---
+++ LibreriaClassi/TEST_DC.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaClassi
+{
+    /// <summary>
+    /// Classe per la gestione delle domande a risposta chiusa
+    /// </summary>
+    internal class TEST_DC
+    {
+        public Guid K_Test_DC { get; set; }
+        public int Numero_Domanda { get; set; }
+        public string Domanda { get; set; }
+        public string Risposte { get; set; }
+        public string RispostaCorretta { get; set; }
+        public string RispostaData { get; set; }
+        public Guid Codice_Test { get; set; }
+
+        public void Inserimento()
+        {
+            DB dB = new DB();
+            dB.query = "TEST_DC_Inserimento";
+            dB.cmd.Parameters.AddWithValue("@Numero_Domanda", Numero_Domanda);
+            dB.cmd.Parameters.AddWithValue("@Domanda", Domanda);
+            dB.cmd.Parameters.AddWithValue("@Risposte", Risposte);
+            dB.cmd.Parameters.AddWithValue("@RispostaCorretta", RispostaCorretta);
+            dB.cmd.Parameters.AddWithValue("@RispostaData", RispostaData);
+            dB.cmd.Parameters.AddWithValue("@Codice_Test", Codice_Test);
+            dB.SQLcommand();
+        }
+        public void Modifica()
+        {
+            DB dB = new DB();
+            dB.query = "TEST_DC_Modifica";
+            dB.cmd.Parameters.AddWithValue("@K_Test_DC", K_Test_DC);
+            dB.cmd.Parameters.AddWithValue("@Numero_Domanda", Numero_Domanda);
+            dB.cmd.Parameters.AddWithValue("@Domanda", Domanda);
+            dB.cmd.Parameters.AddWithValue("@Risposte", Risposte);
+            dB.cmd.Parameters.AddWithValue("@RispostaCorretta", RispostaCorretta);
+            dB.cmd.Parameters.AddWithValue("@RispostaData", RispostaData);
+            dB.cmd.Parameters.AddWithValue("@Codice_Test", Codice_Test);
+
+            dB.SQLcommand();
+        }
+        public DataTable SelezionaTutto()
+        {
+
+            DB dB = new DB();
+            dB.query = "TEST_DC_SelezionaTutto";
+            return dB.SQLselect();
+        }
+        public DataTable SelezionaChiave()
+        {
+            DB dB = new DB();
+            dB.query = "TEST_DC_SelezionaChiave";
+            dB.cmd.Parameters.AddWithValue("@K_Test_DC", K_Test_DC);
+            return dB.SQLselect();
+        }
+    }
+}
