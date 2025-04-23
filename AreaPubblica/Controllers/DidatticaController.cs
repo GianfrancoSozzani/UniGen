a85a94435621baa278a37f8debf9633022ab5a93
vitodebs <56639356+vitodebs@users.noreply.github.com>
2025-04-23 17:54:02 +0200

    Aggiunti solo i controller per l'Area Publica

---
+++ AreaPubblica/Controllers/DidatticaController.cs
@@ -0,0 +1,6 @@
+﻿namespace AreaPubblica.Controllers
+{
+    public class DidatticaController
+    {
+    }
+}
---
+++ AreaPubblica/Controllers/OffertaFormativaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaPubblica.Controllers
+{
+    public class OffertaFormativaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
