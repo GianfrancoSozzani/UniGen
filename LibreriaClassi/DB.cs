7d9b2bd46d1ce3e14d27a747809ba94c5a4b99d8
Diego <diego.negri@hotmail.it>
2025-04-23 16:42:06 +0200

    Aggiunto DB

---
+++ LibreriaClassi/DB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaClassi
+{
+    public class DB
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlDataAdapter DA = new SqlDataAdapter();
+
+        public SqlCommand cmd = new SqlCommand();
+        public string query;
+
+        public DB()
+        {
+            conn.ConnectionString = "Data source=DESKTOP-7PI8964\\SQLEXPRESS;Initial Catalog=AUTOSALONI;Integrated Security=true;";
+            //conn.ConnectionString = "Data source=MSSQLSERVER2019\\SQLEXPRESS;Initial Catalog=AUTOSALONI;Integrated Security=true;";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+        }
+
+        public DataTable SQLselect()
+        {
+            DataTable DT = new DataTable();
+            cmd.CommandText = query;
+            DA.SelectCommand = cmd;
+            DA.Fill(DT);
+            return DT;
+        }
+
+        public void SQLcommand()
+        {
+            cmd.CommandText = query;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+    }
+}
---
+++ LibreriaClassi/FACOLTA.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LibreriaClassi
+{
+    internal class FACOLTA
+    {
+        public Guid K_Facolta { get; set; }
+        public string TitoloFacolta { get; set; }
+
+        public FACOLTA()
+        {
+             
+        }
+
+        // Inserimento
+        public void Inserimento()
+        {
+            DB db = new DB();
+            db.query = "";
+            db.cmd.Parameters.AddWithValue("",);
+            db.SQLcommand();
+        }
+
+        // Seleziona tutti
+        public DataTable SelezionaTutti()
+        {
+            DB dB = new DB();
+            dB.query = "MARCHE_SelectAll";
+            return dB.SQLselect();
+        }
+
+        // Seleziona per chiave
+        public DataTable SelezionaChiave()
+        {
+            DB db = new DB();
+            db.query = "MARCHE_SelezionaChiave";
+            db.cmd.Parameters.AddWithValue("@chiave", K_Marca);
+            return db.SQLselect();
+        }
+
+        // Modifica
+        public void Modifica()
+        {
+            DB dB = new DB();
+            dB.query = "MARCHE_Update";
+            dB.cmd.Parameters.AddWithValue("@chiave", K_Marca);
+            dB.cmd.Parameters.AddWithValue("@marca", Marca);
+            dB.SQLcommand();
+        }
+    }
+}
