31e58e4d65211fb789ec5b7ab0dd359c907d4fa4
yuliyastepanyuk <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-04-24 12:46:34 +0200

    Class Studente created
    Created class for Studente and added class controller Srudenti

---
+++ AreaStudente/Models/Entities/Studente.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AreaStudente.Models.Entities
+{
+    public class Studente
+    {
+        [Key]
+        public Guid K_Studente { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+        public string? Cognome { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataNascita { get; set; }
+        public string? Indirizzo { get; set; }
+        public string? CAP { get; set; }
+        public string? Citta { get; set; }
+        public string? Provincia { get; set; }
+        public byte[]? ImmagineProfilo { get; set; }
+        public string? Tipo { get; set; } 
+        public int? Matricola { get; set; }
+        public DateTime? DataImmatricolazione { get; set; }
+        public string Abilitato { get; set; }
+
+        [ForeignKey("K_Corso")]
+        public Guid? K_Corso { get; set; }
+
+
+
+    }
+}
