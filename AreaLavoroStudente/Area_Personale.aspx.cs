fda47e7045954e83095f787764bad76c6621ff12
Antard11 <antoardito1997@gmail.com>
2025-04-24 10:34:14 +0200

    t

--- AreaLavoroStudente/Area_Personale.aspx.cs
+++ AreaLavoroStudente/Area_Personale.aspx.cs
@@ -1,48 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
 using System.Web.UI.WebControls;
 
 public partial class AreaPersonale : System.Web.UI.Page
 {
-    // Stringa di connessione al database SQL Server
-    private static readonly string connectionString = "Data Source=.;Initial Catalog=GENERATION;Integrated Security=True"; // Modificare in base alla configurazione del tuo SQL Server
-
     protected void Page_Load(object sender, EventArgs e)
     {
-        // Popola la dropdown delle facoltà solo al primo caricamento della pagina
+        // Al primo caricamento della pagina, carica le facoltà
         if (!IsPostBack)
         {
-            CaricaFacolta(); // Chiama metodo per caricare le facoltà dal DB
+            CaricaFacolta();
             ddlFacolta.Items.Insert(0, new ListItem("-- Seleziona una facoltà --", ""));
             ddlCorsi.Items.Insert(0, new ListItem("-- Seleziona un corso --", ""));
         }
     }
 
-    // Metodo per ottenere l'elenco delle facoltà dal database tramite stored procedure
+    // Carica le facoltà usando la classe DB e la stored procedure sp_GetFacolta
     private void CaricaFacolta()
     {
-        using (SqlConnection conn = new SqlConnection(connectionString))
-        using (SqlCommand cmd = new SqlCommand("sp_GetFacolta", conn)) // Stored procedure: sp_GetFacolta
-        {
-            cmd.CommandType = CommandType.StoredProcedure;
-            conn.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            ddlFacolta.DataSource = reader;
-            ddlFacolta.DataTextField = "TitoloFacolta"; // Nome da mostrare
-            ddlFacolta.DataValueField = "K_Facolta"; // Valore da usare per query successive
-            ddlFacolta.DataBind();
-        }
+        DB db = new DB();
+        db.query = "Facolta_SelectAll";
+        DataTable dt = db.SQLselect();
+
+        ddlFacolta.DataSource = dt;
+        ddlFacolta.DataTextField = "TitoloFacolta"; // Colonna visibile
+        ddlFacolta.DataValueField = "K_Facolta";    // Chiave primaria
+        ddlFacolta.DataBind();
     }
 
-    // Evento scatenato dalla selezione di una facoltà
+    // Quando l'utente seleziona una facoltà
     protected void ddlFacolta_SelectedIndexChanged(object sender, EventArgs e)
     {
         ddlCorsi.Items.Clear();
-        CaricaCorsi(ddlFacolta.SelectedValue); // Carica corsi associati alla facoltà selezionata
+        CaricaCorsi(ddlFacolta.SelectedValue);
         ddlCorsi.Items.Insert(0, new ListItem("-- Seleziona un corso --", ""));
+
+        // Reset dei repeater e etichetta
         lblCorso.Text = "";
         rptLezioni.DataSource = null;
         rptLezioni.DataBind();
@@ -50,30 +44,26 @@ public partial class AreaPersonale : System.Web.UI.Page
         rptMateriali.DataBind();
     }
 
-    // Metodo per ottenere i corsi associati a una facoltà
+    // Carica i corsi relativi alla facoltà selezionata
     private void CaricaCorsi(string facoltaId)
     {
-        using (SqlConnection conn = new SqlConnection(connectionString))
-        using (SqlCommand cmd = new SqlCommand("sp_GetCorsiByFacolta", conn)) // Stored procedure: sp_GetCorsiByFacolta
-        {
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@FacoltaId", Guid.Parse(facoltaId));
-            conn.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            ddlCorsi.DataSource = reader;
-            ddlCorsi.DataTextField = "Nome";
-            ddlCorsi.DataValueField = "Id";
-            ddlCorsi.DataBind();
-        }
+        DB db = new DB();
+        db.cmd.Parameters.AddWithValue("@FacoltaId", Guid.Parse(facoltaId));
+        db.query = "sp_GetCorsiByFacolta";
+        DataTable dt = db.SQLselect();
+
+        ddlCorsi.DataSource = dt;
+        ddlCorsi.DataTextField = "Nome";
+        ddlCorsi.DataValueField = "Id";
+        ddlCorsi.DataBind();
     }
 
-    // Evento scatenato dalla selezione di un corso
+    // Quando l'utente seleziona un corso
     protected void ddlCorsi_SelectedIndexChanged(object sender, EventArgs e)
     {
         string corsoId = ddlCorsi.SelectedValue;
         lblCorso.Text = "Corso: " + ddlCorsi.SelectedItem.Text;
 
-        // Popola i repeater con lezioni e materiali
         rptLezioni.DataSource = GetLezioni(corsoId);
         rptLezioni.DataBind();
 
@@ -81,61 +71,57 @@ public partial class AreaPersonale : System.Web.UI.Page
         rptMateriali.DataBind();
     }
 
-    // Metodo per ottenere le lezioni associate al corso selezionato
+    // Ottieni lezioni tramite stored procedure sp_GetLezioniByCorso
     private List<Lezione> GetLezioni(string corsoId)
     {
         var lezioni = new List<Lezione>();
-        using (SqlConnection conn = new SqlConnection(connectionString))
-        using (SqlCommand cmd = new SqlCommand("sp_GetLezioniByCorso", conn)) // Stored procedure: sp_GetLezioniByCorso
+        DB db = new DB();
+        db.cmd.Parameters.AddWithValue("@CorsoId", corsoId);
+        db.query = "sp_GetLezioniByCorso";
+        DataTable dt = db.SQLselect();
+
+        foreach (DataRow row in dt.Rows)
         {
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@CorsoId", corsoId);
-            conn.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+            lezioni.Add(new Lezione
             {
-                lezioni.Add(new Lezione
-                {
-                    Titolo = reader["Titolo"].ToString(),
-                    Vista = Convert.ToBoolean(reader["Vista"])
-                });
-            }
+                Titolo = row["Titolo"].ToString(),
+                Vista = Convert.ToBoolean(row["Vista"])
+            });
         }
+
         return lezioni;
     }
 
-    // Metodo per ottenere i materiali didattici associati al corso selezionato
+    // Ottieni materiali tramite stored procedure sp_GetMaterialiByCorso
     private List<Materiale> GetMateriali(string corsoId)
     {
         var materiali = new List<Materiale>();
-        using (SqlConnection conn = new SqlConnection(connectionString))
-        using (SqlCommand cmd = new SqlCommand("sp_GetMaterialiByCorso", conn)) // Stored procedure: sp_GetMaterialiByCorso
+        DB db = new DB();
+        db.cmd.Parameters.AddWithValue("@CorsoId", corsoId);
+        db.query = "sp_GetMaterialiByCorso";
+        DataTable dt = db.SQLselect();
+
+        foreach (DataRow row in dt.Rows)
         {
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@CorsoId", corsoId);
-            conn.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+            materiali.Add(new Materiale
             {
-                materiali.Add(new Materiale
-                {
-                    Titolo = reader["Titolo"].ToString(),
-                    UrlFile = reader["UrlFile"].ToString(),
-                    Tipo = reader["Tipo"].ToString()
-                });
-            }
+                Titolo = row["Titolo"].ToString(),
+                UrlFile = row["UrlFile"].ToString(),
+                Tipo = row["Tipo"].ToString()
+            });
         }
+
         return materiali;
     }
 
-    // Classe che rappresenta una lezione
+    // Classe Lezione
     public class Lezione
     {
         public string Titolo { get; set; }
         public bool Vista { get; set; }
     }
 
-    // Classe che rappresenta un materiale didattico
+    // Classe Materiale
     public class Materiale
     {
         public string Titolo { get; set; }
