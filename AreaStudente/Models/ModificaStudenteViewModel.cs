cf5e374fd4fdf3d0d1205191669ee89b199bcb0b
Barbarasnewpath <barbarasnewpath@gmail.com>
2025-04-24 13:06:39 +0200

    Aggiunta ViewModel

---
+++ AreaStudente/Models/ModificaStudenteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AreaStudente.Models
+{
+    public class ModificaStudenteViewModel
+    {
+        
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Cognome { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataNascita { get; set; }
+        public string? Indirizzo { get; set; }
+        public string? CAP { get; set; }
+        public string? Citta { get; set; }
+        public string? Provincia { get; set; }
+
+        
+       
+       
+    }
+}
