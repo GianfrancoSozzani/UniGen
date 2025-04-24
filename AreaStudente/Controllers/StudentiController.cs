da5ce089fc8183f90dd81c1948a80d8f8e5f5a14
yuliyastepanyuk <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-04-24 13:01:39 +0200

    Created view for StudentiController as Show

---
+++ AreaStudente/Controllers/StudentiController.cs
@@ -0,0 +1,25 @@
+﻿using AreaStudente.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AreaStudente.Controllers
+{
+    public class StudentiController : Controller
+    {
+
+        private readonly ApplicationDbContext dbContext; // Sto staziando il contesto del database
+        public StudentiController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext; // Inizializzo il contesto del database 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
