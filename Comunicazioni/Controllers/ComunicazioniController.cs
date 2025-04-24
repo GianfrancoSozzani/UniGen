b3a470018fb7d512d9131710c095e209997d1b6a
RosaMaffei <mffrso99@gmail.com>
2025-04-24 11:47:11 +0200

    WorkinProgress...Add in ComunicazioniController

--- Comunicazioni/Controllers/ComunicazioniController.cs
+++ Comunicazioni/Controllers/ComunicazioniController.cs
@@ -1,4 +1,6 @@
 ﻿using Comunicazioni.Data;
+using Comunicazioni.Models;
+using Comunicazioni.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,7 +23,42 @@ namespace Comunicazioni.Controllers
             return View(comunicazioni);
         }
 
+        //----------------------------------------------//
+        //ADD-------------------------------------------//
+        //----------------------------------------------//
+        [HttpGet] //visualizzo i dati
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]  //ricevo i dati dalla lista ed inserisco nel db
+        public async Task<IActionResult>Add(AddComunicazioneViewModel viewModel)
+        {
+            var comunicazione = new Comunicazione
+            {
+                DataOraComunicazione = viewModel.DataOraComunicazione,
+                Soggetto = viewModel.Soggetto,
+                Testo = viewModel.Testo,
+                K_Studente = viewModel.K_Studente,
+                K_Docente = viewModel.K_Docente,
+            };
 
+            //collegamento tra studenti e facoltà
+            foreach (var riga in comunicazione)
+            {
+                // var (=> quello che io sto cercando) sarà uguale
+                riga.Studente = dbContext.Facolta.FirstOrDefault(u => u.K_Studente == riga.K_Studente); //una join
+                riga.Docente = dbContext.Facolta.FirstOrDefault(u => u.K_Docente == riga.K_Docente); //una join
+
+            }
+
+            return View(comunicazione);
+            await dbContext.Comunicazioni.AddAsync(comunicazione);
+            await dbContext.SaveChangesAsync();
+            return RedirectToAction("Listcshtml", "Comunicazioni");
+        }
+   
+        }
 
     }
 }
