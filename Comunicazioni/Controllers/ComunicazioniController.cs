12b67905087d8c01e9aeb80364ecefe8dd7e3a10
GianfrancoSozzani <gianfranco.sozzani@gmail.com>
2025-04-24 13:05:23 +0200

    d

--- Comunicazioni/Controllers/ComunicazioniController.cs
+++ Comunicazioni/Controllers/ComunicazioniController.cs
@@ -1,4 +1,5 @@
 ﻿using Comunicazioni.Data;
+using Comunicazioni.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,60 @@ namespace Comunicazioni.Controllers
         //----------------------------------------------//
         //LIST------------------------------------------//
         //----------------------------------------------//
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            if (amministrazione)
+            {
+
+                var comunicazioni = await dbContext.Comunicazioni.ToListAsync();
+                return View(comunicazioni);
+            }
+
+            if (docente)
+            {
+                var comunicazioni = await dbContext.Comunicazioni.FindAsync(esame);
+                await dbContext.Comunicazioni.ToListAsync();
+                return View(comunicazioni);
+            }
+            else
+            {
+                var comunicazioni = await dbContext.Comunicazioni.FindAsync(corso);
+                await dbContext.Comunicazioni.ToListAsync();
+                return View(comunicazioni);
+            }
+
+        }
+
+        //----------------------------------------------//
+        //ADD-------------------------------------------//
+        //----------------------------------------------//
+        [HttpGet] //visualizzo i dati
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]  //ricevo i dati dalla lista ed inserisco nel db
+        public async Task<IActionResult> Add(AddComunicazioneViewModel viewModel)
+        {
+            var comunicazione = new Comunicazione
+            {
+                DataOraComunicazione = viewModel.DataOraComunicazione,
+                Soggetto = viewModel.Soggetto,
+                Testo = viewModel.Testo,
+                K_Studente = viewModel.K_Studente,
+                K_Docente = viewModel.K_Docente,
+            };
+
+            //collegamento tra comunicazioni e studente-docente
+            comunicazione.Studente = dbContext.Studenti.FirstOrDefault(s => s.K_Studente == comunicazione.K_Studente); //una join
+            comunicazione.Docente = dbContext.Docenti.FirstOrDefault(d => d.K_Docente == comunicazione.K_Docente); //una join
+
+            await dbContext.Comunicazioni.AddAsync(comunicazione);
+            await dbContext.SaveChangesAsync();
+            return RedirectToAction("Listcshtml", "Comunicazioni");
+        }
 
     }
 
