1ca8246a98782bacccbf516164fe051df52414c3
Fabiopterix <fabiogiuliv@gmail.com>
2025-04-24 13:13:49 +0200

    comunicazioni

---
+++ AreaStudente/Controllers/ComunicazioniController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaStudente.Controllers
+{
+    public class ComunicazioniController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
