0b7c075c0f1e0b3d09f1760af72db951fff6057a
GianfrancoSozzani <gianfranco.sozzani@gmail.com>
2025-04-26 12:56:54 +0200

    funziona

--- Comunicazioni/Controllers/ComunicazioniController.cs
+++ Comunicazioni/Controllers/ComunicazioniController.cs
@@ -25,6 +25,8 @@ namespace Comunicazioni.Controllers
                 .Include(c => c.Studente)
                 .Include(c => c.Docente)
                 .Include(c => c.Esami)
+                .OrderBy(c => c.DataOraComunicazione)
+                .GroupBy(c => c.Codice_Comunicazione)
                 .ToListAsync();
             return View(comunicazioni);
         }
@@ -62,6 +64,46 @@ namespace Comunicazioni.Controllers
                 K_Soggetto = Guid.Parse(HttpContext.Session.GetString("chiave")),
                 K_Esame = viewModel.K_Esame
             };
+
+            if (ruolo == "Operatore")
+            {
+                comunicazione.Soggetto = "A";
+            }
+            else if (ruolo == "Docente")
+            {
+                comunicazione.Soggetto = "D";
+                comunicazione.K_Docente = Guid.Parse(HttpContext.Session.GetString("chiave"));
+            }
+            else if (ruolo == "Studente")
+            {
+                comunicazione.Soggetto = "S";
+                comunicazione.K_Studente = Guid.Parse(HttpContext.Session.GetString("chiave"));
+            }
+
+            //collegamento tra comunicazioni e studente-docente
+            comunicazione.Studente = dbContext.Studenti.FirstOrDefault(s => s.K_Studente == comunicazione.K_Studente); //una join
+            comunicazione.Docente = dbContext.Docenti.FirstOrDefault(d => d.K_Docente == comunicazione.K_Docente); //una join
+            comunicazione.Esami = dbContext.Esami.FirstOrDefault(e => e.K_Esame == comunicazione.K_Esame);
+
+            await dbContext.Comunicazioni.AddAsync(comunicazione);
+            await dbContext.SaveChangesAsync();
+            return RedirectToAction("List", "Comunicazioni");
+        }
+
+
+        public async Task<IActionResult> AddRisposta(Comunicazione viewModel)
+        {
+            string ruolo = HttpContext.Session.GetString("ruolo");
+
+            var comunicazione = new Comunicazione
+            {
+                Codice_Comunicazione = viewModel.Codice_Comunicazione,
+                DataOraComunicazione = DateTime.Now,
+                Testo = viewModel.Testo,
+                K_Soggetto = Guid.Parse(HttpContext.Session.GetString("chiave")),
+                K_Esame = viewModel.K_Esame
+            };
+
             if (ruolo == "Operatore")
             {
                 comunicazione.Soggetto = "A";
