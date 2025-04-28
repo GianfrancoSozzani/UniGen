5157c45de168154723a3fcc12c8ae4edb9f78701
Luca3452 <luca.malini7@gmail.com>
2025-04-28 15:27:45 +0200

    Inizio Materiali

---
+++ AreaDocente/Models/AddMaterialiViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AreaDocente.Models;
+using Microsoft.AspNetCore.Http;
+public class AddMaterialiViewModel
+{
+    public string Titolo { get; set; }
+    public IFormFile materiale { get; set; }
+    public string Tipo { get; set; }
+
+}
+
