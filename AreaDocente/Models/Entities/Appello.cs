b9ebe65bdaee985461900d00861d6c2f0e7de9e2
Krystian Kamil Grupa <25132033+KriiXOne@users.noreply.github.com>
2025-04-28 14:23:49 +0200

    Merge pt.2

--- AreaDocente/Models/Entities/Appello.cs
+++ AreaDocente/Models/Entities/Appello.cs
@@ -1,21 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AreaDocente.Models.Entities
 {
-    public class Appello
+    public class APPELLO
     {
         [Key]
         public Guid K_Appello { get; set; }
         public Guid K_Esame { get; set; }
         [ForeignKey("K_Esame")]
-        [ValidateNever]
+        public ESAME Esame { get; set; }
         public DateTime DataAppello { get; set; }
         public DateTime DataVerbalizzazione { get; set; }
+        /// <summary>
+        /// Tipo di appello: O = Orale, S=Scritto, L=Laurea
+        /// </summary>
         public char Tipo { get; set; }
         public string Link { get; set; }
         public DateTime DataOrale { get; set; }
-        public Esame Esame { get; set; }
     }
 }
--- AreaDocente/Models/Entities/Esame.cs
+++ AreaDocente/Models/Entities/Esame.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AreaDocente.Models.Entities
 {
-    public class Esame
+    public class ESAME
     {
         [Key]
         public Guid K_Esame { get; set; }
-        public Guid K_PianoStudio { get; set; }
-        [ForeignKey("K_PianoStudio")]
-        [ValidateNever]
+        //public Guid K_PianoStudio { get; set; }
+        //[ForeignKey("K_PianoStudio")]
+        //public PIANOSTUDIO PianoStudio { get; set; }
         public string TitoloEsame { get; set; }
-        public int CFU { get; set; }
-        [ForeignKey("K_Docente")]
-        [ValidateNever]
         public Guid K_Docente { get; set; }
+        [ForeignKey("K_Docente")]
+        public DOCENTE Docente { get; set; }
+        public int CFU { get; set; }
     }
 }
