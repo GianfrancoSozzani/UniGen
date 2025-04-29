23a769dda008f8bbb1553f10caea301d325fc229
Barbarasnewpath <barbarasnewpath@gmail.com>
2025-04-29 11:30:54 +0200

    merged

--- AreaPubblica/Controllers/FAQController.cs
+++ AreaPubblica/Controllers/FAQController.cs
@@ -3,10 +3,13 @@
 namespace AreaPubblica.Controllers
 {
     public class FAQController : Controller
+    public class FaqController : Controller
     {
         public IActionResult Index()
+        public IActionResult Faq()
         {
             return View();
+            return View("Faq", "Faq");
         }
     }
 }
