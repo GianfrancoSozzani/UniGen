3434462ba021c51285d739c97f1a293fc12fac98
MatteoBrus778 <matteobrs24@gmail.com>
2025-04-29 10:07:53 +0200

    Update FaqController.cs

--- AreaPubblica/Controllers/FaqController.cs
+++ AreaPubblica/Controllers/FaqController.cs
@@ -2,11 +2,8 @@
 
 namespace AreaPubblica.Controllers
 {
-    public class FaqController : Controller
     {
-        public IActionResult Faq()
         {
-            return View("Faq", "Faq");
         }
     }
 }
