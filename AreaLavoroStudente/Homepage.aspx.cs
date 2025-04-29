bc46317e4478e795cdb1f1aee50e3f624b0fc9fc
eleonoracordella <ec.elettra@gmail.com>
2025-04-29 15:27:29 +0200

    Ultimato Homepage e MasterPage

--- AreaLavoroStudente/Homepage.aspx.cs
+++ AreaLavoroStudente/Homepage.aspx.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LibreriaClassi;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public int matricolaTest;
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if (Session["Matricola"] != null)
+        //{
+        //    int matricolaTest = Convert.ToInt32(Session["Matricola"]);
+        //    CaricaAA(matricolaTest);
+        //}
+        //else
+        //{
+        //    // Gestisci il caso in cui la matricola non è presente nella sessione
+        //    Response.Write("Utente non trovato.");
+        //    Response.Redirect("~/Login.aspx");
+
+        //}
+        if (!IsPostBack)
+        {
+
+            string abilitazione = "N";
+                /*Session["Abilitazione"]?.ToString();*/ 
+            if (abilitazione == "N")
+            { divLezioni.Visible = (abilitazione == "admin");
+            divComunicazioni.Visible = (abilitazione == "admin");
+            divAppelli.Visible = (abilitazione == "admin");
+
+            }
+
+            // Nasconde la card se non sei admin
+           
+        }
+
+        CaricaAA(matricolaTest);
 
     }
+
+public void CaricaAA(int matricolaTest)
+{
+    matricolaTest = 1;
+    STUDENTI studente = new STUDENTI();
+    studente.Matricola = matricolaTest;
+    DataTable dt = studente.SelezionaAnnoAccademico();
+
+    if (dt.Rows.Count == 1)
+    {
+        string annoAccademico = dt.Rows[0]["AnnoAccademico"].ToString();
+        string corso = dt.Rows[0]["TitoloCorso"].ToString();
+        string facolta = dt.Rows[0]["TitoloFacolta"].ToString();
+
+        lblAnno.Text = "Anno Accademico " + annoAccademico;
+        lblCorso.Text = corso;
+        lblFacolta.Text = facolta;
+        }
+}
 }
--- AreaLavoroStudente/MasterPage.master.cs
+++ AreaLavoroStudente/MasterPage.master.cs
@@ -19,12 +19,49 @@ public partial class MasterPage : System.Web.UI.MasterPage
     public int matricolaTest;
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+
+            string abilitazione = "N";
+            /*Session["Abilitazione"]?.ToString();*/
+            if (abilitazione == "N")
+            {
+                liAppelli.Visible = (abilitazione == "admin");
+                liMateriali.Visible = (abilitazione == "admin");
+                liComunicazioni.Visible = (abilitazione == "admin");
+            }
+
+            // Nasconde la card se non sei admin
+
+        }
+
+
+        // Verifica che la sessione contenga una matricola valida
+        //if (Session["matricola"] == null || !int.TryParse(Session["matricola"].ToString(), out matricolaTest) || matricolaTest == 0)
+        //{
+        //    Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Utente non loggato');", true);
+        //    Response.Redirect("~/Login.aspx");
+        //  
+        //}
+
+        //STUDENTI studente = new STUDENTI();
+        //studente.Matricola = matricolaTest;
+        //DataTable dt = studente.SelezionaPerMatricola();
+
+        //if (dt.Rows.Count == 1)
+        //{
+        //    string nome = dt.Rows[0]["Nome"].ToString();
+        //    string cognome = dt.Rows[0]["Cognome"].ToString();
+        //    lblStudente.Text = nome + " " + cognome;
+        //}
+
         matricolaTest = 2;
 
         if (matricolaTest == 0)
         {
             Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Utente non loggato');", true);
-            return;
+            Response.Redirect("~/Login.aspx");
+            
         }
 
         STUDENTI studente = new STUDENTI();
@@ -37,6 +74,16 @@ public partial class MasterPage : System.Web.UI.MasterPage
             string cognome = dt.Rows[0]["Cognome"].ToString();
             lblStudente.Text = nome + " " + cognome;
         }
+
+        
+
+
+
+
+
+
+
+
     }
 }
 
