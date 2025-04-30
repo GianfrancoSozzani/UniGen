534891a45109f1c418f916346a17b18788b00bce
Luigi D'Esposito <superkiku06@gmail.com>
2025-04-30 14:45:50 +0200

    Aggiunti ? classi

--- AreaDocente/Models/EditMaterialiViewModel.cs
+++ AreaDocente/Models/EditMaterialiViewModel.cs
@@ -9,10 +9,10 @@ namespace AreaDocente.Models
     {
         [Key]
         public Guid K_Materiale { get; set; }
-        public string Titolo { get; set; }
-        public byte[] Materiale { get; set; }
-        public IFormFile MaterialeDA { get; set; }
-        public string Tipo { get; set; }
-        public Guid K_Esame { get; set; }
+        public string? Titolo { get; set; }
+        public byte[]? Materiale { get; set; }
+        public IFormFile? MaterialeDA { get; set; }
+        public string? Tipo { get; set; }
+        public Guid? K_Esame { get; set; }
     }
 }
--- AreaDocente/Models/Entities/MVCAppello.cs
+++ AreaDocente/Models/Entities/MVCAppello.cs
@@ -7,14 +7,14 @@ namespace AreaDocente.Models.Entities
     {
         [Key]
         public Guid K_Appello { get; set; }
-        public Guid K_Esame { get; set; }
+        public Guid? K_Esame { get; set; }
 
         [ForeignKey("K_Esame")]
-        public MVCEsame Esame { get; set; }
-        public DateTime DataAppello { get; set; }
-        public DateTime DataVerbalizzazione { get; set; }
-        public char Tipo { get; set; }
-        public string Link { get; set; }
-        public DateTime DataOrale { get; set; }
+        public MVCEsame? Esame { get; set; }
+        public DateTime? DataAppello { get; set; }
+        public DateTime? DataVerbalizzazione { get; set; }
+        public char? Tipo { get; set; }
+        public string? Link { get; set; }
+        public DateTime? DataOrale { get; set; }
     }
 }
