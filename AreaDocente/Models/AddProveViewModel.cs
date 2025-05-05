ad43b32446f29df3cd7e28991435d322897542a6
Luigi D'Esposito <superkiku06@gmail.com>
2025-05-05 14:15:22 +0200

    s

--- AreaDocente/Models/AddProveViewModel.cs
+++ AreaDocente/Models/AddProveViewModel.cs
@@ -2,6 +2,10 @@
 {
     public class AddProveViewModel
     {
-
+        public Guid K_Prova { get; set; }
+        public Guid? K_Appello { get; set; }
+        public string? Link { get; set; }
+        public string? Tipologia { get; set; }
+        public Guid? K_Esame { get; set; }
     }
 }
