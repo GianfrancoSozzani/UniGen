a3627499d5421668c451df647f8a59711d1cfe44
Fabiopterix <fabiogiuliv@gmail.com>
2025-05-05 10:44:16 +0200

    Revert "Merge branch 'AreaLavoroStudente' of https://github.com/GianfrancoSozzani/UniGen into AreaLavoroStudente"
    This reverts commit e9c45f43c61939e8a2b4657cd99bf23294708b25, reversing
    changes made to 47b94ebebb087fa15d0c76903f5f83c2b1ec7bb5.

--- AreaDocente/Models/AddLezioniViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AreaDocente.Models
-{
-    public class AddLezioniViewModel
-    {
-        public string? Titolo { get; set; }
-        public string? Video { get; set; }
-        public Guid? K_Esame {  get; set; }
-    }
-}
--- AreaDocente/Program.cs
+++ AreaDocente/Program.cs
@@ -1,5 +1,3 @@
-using AreaDocente.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace AreaDocente
 {
@@ -10,32 +8,27 @@ namespace AreaDocente
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(builder.Configuration.GetConnectionString("UniGenConn"))
-                );
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllers();
 
             app.Run();
         }
