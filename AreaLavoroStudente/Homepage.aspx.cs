ae0ce3497ce6e40414ce2b3992b31a4ad8a8f4b6
GianfrancoSozzani <gianfranco.sozzani@gmail.com>
2025-05-07 12:45:31 +0200

    Revert "Merge branch 'Comunicazioni' into AreaLavoroStudente"
    This reverts commit 224d667f0e8a9c1bc2df2194861197b26ffb2d3c, reversing
    changes made to 7db3a968086305094dff76d830eef693da5dffdd.

--- AreaLavoroStudente/Homepage.aspx.cs
+++ AreaLavoroStudente/Homepage.aspx.cs
@@ -10,7 +10,7 @@ using LibreriaClassi;
 public partial class _Default : System.Web.UI.Page
 {
 
-
+   
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -20,19 +20,18 @@ public partial class _Default : System.Web.UI.Page
             string Matricola = Session["mat"] as string;
             string Abilitazione = Session["a"] as string;
             string Ruolo = Session["r"] as string;
-
+           
 
             // 2. Se mancano, prova dalla QueryString
-            if (string.IsNullOrEmpty(K_Studente) || string.IsNullOrEmpty(Matricola) || string.IsNullOrEmpty(Abilitazione) || string.IsNullOrEmpty(Ruolo))
+            if (string.IsNullOrEmpty(K_Studente) || string.IsNullOrEmpty(Matricola) || string.IsNullOrEmpty(Abilitazione))
             {
                 K_Studente = Request.QueryString["cod"];
                 Matricola = Request.QueryString["mat"];
                 Abilitazione = Request.QueryString["a"];
-                Ruolo = Request.QueryString["r"];
 
 
                 // Se ora li trovi, salvali nella sessione
-                if (!string.IsNullOrEmpty(K_Studente) && !string.IsNullOrEmpty(Matricola) && !string.IsNullOrEmpty(Abilitazione) || string.IsNullOrEmpty(Ruolo))
+                if (!string.IsNullOrEmpty(K_Studente) && !string.IsNullOrEmpty(Matricola) && !string.IsNullOrEmpty(Abilitazione))
                 {
                     Session["cod"] = K_Studente;
                     Session["mat"] = Matricola;
@@ -92,15 +91,19 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnComunicazioni_Click(object sender, EventArgs e)
     {
-        string Ruolo;
-        string Cod;
-    
-        Ruolo = Session["r"].ToString();
-        Cod = Session["cod"].ToString(); 
+        string Ruolo = "";
+        string Cod = "";
+
+        if (Session["r"] != null && Session["cod"] != null)
+        {
+            string chiave = Session["r"].ToString(); // Es. "ruolo"
+            Ruolo = Request.QueryString[chiave];     // Es. valore da ?ruolo=admin
+            Cod = Session["cod"].ToString();         // Es. "12345"
+        }
 
         string url = "https://localhost:7098/Comunicazioni/List?cod=" + Cod +
-                 "&r=" + Ruolo;
-        Response.Redirect(url);
+                     "&r=" + Ruolo;
 
+        Response.Redirect(url);
     }
 }
